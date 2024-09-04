4b642b38f6851e681a017bf7a10adc55641617fe
Tina Bell Vance <tina@bellvance.com>
2024-09-03 20:35:16 -0400

    Add import/export

--- gallery/admin.py
+++ gallery/admin.py
@@ -3,18 +3,41 @@ from django.contrib import admin
 from .models import Image, Series, Medium
 from import_export import resources
 
-admin.site.register(Series)
 
-class SeriesAdmin(admin.ModelAdmin):
+class SeriesResource(resources.ModelResource):
+  class Meta:
+    model = Series
+    import_id_fields = ["title", "cover_image", "description", "medium", "is_featured"]
+    skip_unchanged = True
+    use_bulk = True
+
+class SeriesAdmin(ImportExportActionModelAdmin):
+  resource_class = SeriesResource
   list_display = ('title', 'slug', 'cover_image')
-  prepopulated_fields = {"slug": ("title")}
+  prepopulated_fields = {"slug": ("title",)}
 
-admin.site.register(Medium)
+admin.site.register(Series, SeriesAdmin)
 
 class MediumAdmin(admin.ModelAdmin):
   list_display = ('name', 'slug', 'cover_image')
   prepopulated_fields = {"slug": ("name")}
 
+class MediumResource(resources.ModelResource):
+  class Meta:
+    model = Medium
+    import_id_fields = ["name", "cover_image", "description", "is_featured"]
+    skip_unchanged = True
+    use_bulk = True
+
+  def get_description(self):
+    return self.description
+
+class MediumAdmin(ImportExportActionModelAdmin):
+  resource_class = MediumResource
+  list_display = ('name', 'slug', 'cover_image')
+  prepopulated_fields = {"slug": ("name",)}
+
+admin.site.register(Medium, MediumAdmin)
 
 class ImageResource(resources.ModelResource):
     class Meta:
