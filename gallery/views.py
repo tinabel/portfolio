83ad4792c9c27e16bb3ca2b1aef40f2e4c769038
Tina Bell Vance <tina@bellvance.com>
2024-09-06 16:14:36 -0400

    Remove position from views

--- gallery/views.py
+++ gallery/views.py
@@ -5,14 +5,14 @@ from .serializers import ImageSerializer
 
 def index(request):
   images = Image.objects.filter(is_featured=True)
-  series = Series.objects.filter(is_featured=True, ordering='position')
+  series = Series.objects.filter(is_featured=True)
   featured_series_image_ids = series.all().values_list('cover_image', flat = True)
   series_images = Image.objects.filter(id__in=featured_series_image_ids)
 
   return render(request, 'index.html', {'featured_images': images, 'featured_series': series, 'featured_series_images': series_images})
 
 def artwork_view(request):
-  all_series = Series.objects.all().order_by('position')
+  all_series = Series.objects.all()
   series_image_ids = all_series.all().values_list('cover_image', flat = True)
   series_images = Image.objects.filter(id__in=series_image_ids)
   return render(request, 'artwork.html', {'series': all_series, 'series_images': series_images})
